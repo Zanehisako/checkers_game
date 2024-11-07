3d08df3b832fd911d078fdbc7489fa6b55d6a09e
Zanehisako <yassinetakiko@gmail.com>
2024-11-07 22:32:13 +0100

    added the board

---
+++ src/app/board.tsx
@@ -0,0 +1,26 @@
+export function Board() {
+  const board_size = 8;
+  const get_Cell_Color = (row: number, column: number) =>
+    (row + column) % 2 == 0 ? "bg-white" : "bg-black";
+
+  const cells = () => {
+    const cells = [];
+    for (let row = 0; row < board_size; row++) {
+      for (let col = 0; col < board_size; col++) {
+        const key: number = col + row * board_size;
+        cells.push(
+          <div
+            key={key}
+            className={`w-full h-full ${get_Cell_Color(row, col)}`}
+          ></div>,
+        );
+      }
+    }
+    return cells;
+  };
+  return (
+    <div className={`grid grid-cols-8 w-96 h-96 border-4 border-black`}>
+      {cells()}
+    </div>
+  );
+}
