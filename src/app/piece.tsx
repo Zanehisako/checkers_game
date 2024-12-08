3b628ca2aff8eb541fec51fa193495ed7d48b66c
Zanehisako <yassinetakiko@gmail.com>
2024-12-08 23:49:31 +0100

    --Now we get the current position of the piece after moving

--- src/app/piece.tsx
+++ src/app/piece.tsx
@@ -23,6 +23,7 @@ export function Piece({
   const [position_x, setX] = useState(x);
   const [position_y, setY] = useState(y);
   const [mouse_x, setMouseX] = useState(0);
+  const [new_index, setIndex] = useState(index);
 
   const handleMouseDown = (event: React.MouseEvent) => {
     const { clientX } = event;
@@ -34,9 +35,11 @@ export function Piece({
     console.log("mouse X:", clientX);
     console.log("mouse Y:", clientY);
     if (clientX - mouse_x > 0) {
+      setIndex(index - 7);
       setX(position_x + 1);
       type == 0 ? setY(position_y - 1) : setY(position_y + 1);
     } else {
+      setIndex(index - 9);
       setX(position_x - 1);
       type == 0 ? setY(position_y - 1) : setY(position_y + 1);
     }
@@ -55,7 +58,11 @@ export function Piece({
       }}
       alt="piece"
       onClick={() =>
-        onSelect(type == 0 ? [index - 7, index - 9] : [index + 7, index + 9])
+        onSelect(
+          type == 0
+            ? [new_index - 7, new_index - 9]
+            : [new_index + 7, new_index + 9],
+        )
       }
       onMouseDown={handleMouseDown}
       onDragEnd={handleMouseUp}
